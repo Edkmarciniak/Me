af8e495ab75e9a7b82938ae8858b3e336c7c0be4
Ed Marciniak <ekmarciniak@gmail.com>
2022-12-18 09:15:47 -0600

    Brocode 4 complete

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
+// how to accept user name input
 
-let username = windows.prompt("Whats your name?")
+// Easy way with a window prompt
+
+/*
+let username = window.prompt("Whats your name?")
 
 console.log(username);
+*/
+
+let username;
+
+document.getElementById("myButton").onclick = function() {
+
+    username = document.getElementById("myText").value;
+    console.log(username);
+    document.getElementById("myLabel").innerHTML = "Hello " + username;
+}
+    
