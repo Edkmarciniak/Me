5eef67a46679231abda153fe34a4ddc84eb60c69
Ed Marciniak <ekmarciniak@gmail.com>
2022-12-18 08:17:38 -0600

    Brocode 4

--- index.js
+++ index.js
@@ -1,16 +1,4 @@
 
-// this is blank line
+let username = windows.prompt("Whats your name?")
 
-
-
-let firstName = "Bro!"; //string
-let age = 25;  //string
-let student = false;  //boolean
-
-console.log("Hello", firstName);
-console.log("You are", age, "years old");
-console.log("Enrolled:", student);
-
-document.getElementById("p1").innerHTML =  "Hello" + firstName;
-document.getElementById("p2").innerHTML = "You are" + age + "years old";
-document.getElementById("p3").innerHTML = "Enrolled:" + student;
+console.log(username);
