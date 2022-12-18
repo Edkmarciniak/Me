35f41c03c68476df6f8cbef06fcbf0b7afa70e27
Ed Marciniak <ekmarciniak@gmail.com>
2022-12-17 20:34:46 -0600

    More form Brocode2

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-let age;
-age = 21;
 
-console.log(age);
+// this is blank line
+
+
+
+let firstName = "Bro!"; //string
+let age = 25;  //string
+let student = false;  //boolean
+
+console.log("Hello", firstName);
+console.log("You are", age, "years old");
+console.log("Enrolled:", student);
+
+document.getElementById("p1").innerHTML =  "Hello" + firstName;
+document.getElementById("p2").innerHTML = "You are" + age + "years old";
+document.getElementById("p3").innerHTML = "Enrolled:" + student;
