99252b3935a3634436bf0fe2323b47e3b483638d
Ed Marciniak <ekmarciniak@gmail.com>
2022-12-19 01:41:13 -0600

    Clean html for next tutorial

--- index.js
+++ index.js
@@ -1,19 +1,2 @@
-// how to accept user name input
 
-// Easy way with a window prompt
-
-/*
-let username = window.prompt("Whats your name?")
-
-console.log(username);
-*/
-
-let username;
-
-document.getElementById("myButton").onclick = function() {
-
-    username = document.getElementById("myText").value;
-    console.log(username);
-    document.getElementById("myLabel").innerHTML = "Hello " + username;
-}
     
